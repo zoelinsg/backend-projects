848f3da6fc66dbab6bd9e931f2edbd5bcf561262
zoelinsg <148110678+zoelinsg@users.noreply.github.com>
2025-01-24 22:37:30 +0800

    Delete django-projects/PIXBlog directory

--- django-projects/PIXBlog/README.md
+++
@@ -1,4 +0,0 @@
-# Django Marketplace project
-## 使用PIXNET痞客幫的Open Data API，來顯示國內旅遊的部落格文章
-## 參考教程
-## https://www.learncodewithmike.com/2020/03/django-bootstrap.html
--- django-projects/PIXBlog/posts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import requests
-
-# Create your views here.
-def index(request):
-    response = requests.get(
-        "https://emma.pixnet.cc/mainpage/blog/categories/hot/28") #28為國內旅遊
-    articles = response.json()["articles"] #轉換為JSON物件後，存取articles欄位
-    return render(request, "posts/index.html", {"articles": articles})
