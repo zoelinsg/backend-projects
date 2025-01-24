607a53e1fae3b0058b20d1cd4f37cb0a636d7ab8
zoelinsg <148110678+zoelinsg@users.noreply.github.com>
2025-01-24 22:37:18 +0800

    Delete django-projects/Photo directory

--- django-projects/Photo/photos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Photo, Category, Album, Tag
-
-# 註冊模型到管理後台
-admin.site.register(Photo)  # 註冊相片模型
-admin.site.register(Category)  # 註冊相片類別模型
-admin.site.register(Album)  # 註冊相簿模型
-admin.site.register(Tag)  # 註冊標籤模型
--- django-projects/Photo/photos/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Photo, Tag
-
-class PhotoForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="使用 # 分隔標籤，例如：#台北 #夜景")
-
-    class Meta:
-        model = Photo
-        fields = ['title', 'description', 'album', 'category', 'image', 'tags']  # 單張照片上傳
